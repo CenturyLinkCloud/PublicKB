f3036952b26fa882d56637847e8d114772a79be9
Keith Resar <keith.resar@centurylinkcloud.com>
2014-12-04 21:26:58 -0600

    -

--- src/generate_windows_exe.bat
+++ src/generate_windows_exe.bat
@@ -1,7 +1,7 @@
 
-#c:\python27\scripts\pyinstaller.exe clc.py
-c:\python27\scripts\pyinstaller.exe --noconfirm --icon=dist\centurylinkcloud.ico --onefile clc.spec
-mv dist\clc\clc.exe dist\
+rem c:\python27\scripts\pyinstaller.exe --noconfirm --icon=dist/centurylinkcloud.ico --clean --onefile clc.py
+c:\python27\scripts\pyinstaller.exe --noconfirm --clean --onefile clc.py
+rem move /Y dist\clc\clc.exe dist\
 
 
 
