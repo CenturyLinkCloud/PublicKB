64599c3b98fa56ee40a84b2e8bcbec9bd80210ed
Keith Resar <keith.resar@centurylinkcloud.com>
2014-12-18 10:57:55 -0600

    -

--- src/generate_docs.sh
+++ src/generate_docs.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 pydoc -w clc clc.{account,api,billing,blueprint,cli,group,network,output,queue,server,shell,user}
-perl -p -i -e "s/keithresar//" *html
+perl -p -i -e "s/keithresar//g" *html
 
 echo git checkout gh-pages
 echo mv *html ../src/
