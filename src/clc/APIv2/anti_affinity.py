42f73b4a5bf2279f0ab9d23544bfc1c4edc6262a
Keith Resar <keith.resar@centurylinkcloud.com>
2014-12-29 17:03:03 -0600

    -

--- src/clc/APIv2/anti_affinity.py
+++ src/clc/APIv2/anti_affinity.py
@@ -18,19 +18,41 @@ import clc
 class AntiAffinity(object):
 
 	@staticmethod
-	def GetAll(alias=None):  
+	def GetAll(alias=None,location=None):  
 		"""Gets a list of anti-affinity policies within a given account.
 
 		https://t3n.zendesk.com/entries/44657214-Get-Anti-Affinity-Policies
 
+		>>> clc.v2.AntiAffinity.GetAll()
+		[<clc.APIv2.anti_affinity.AntiAffinity object at 0x10c65e910>, <clc.APIv2.anti_affinity.AntiAffinity object at 0x10c65ec90>]
+
 		"""
 		if not alias:  alias = clc.v2.Account.GetAlias()
-		r = clc.v2.API.Call('GET','antiAffinityPolicies/%s' % alias,{})
+
+		policies = []
+		for r in clc.v2.API.Call('GET','antiAffinityPolicies/%s' % alias,{}):
+			if location and r['location'].lower()!=location.lower():  continue
+			servers = [obj['id'] for obj in r['links'] if obj['rel'] == "server"]
+			policies.append(AntiAffinity(id=r['id'],name=r['name'],location=r['location'],servers=servers))
+
+		return(policies)
 
 	
-	#def GetLocation(location=None,alias=None):
+	@staticmethod
+	def GetLocation(location=None,alias=None):
+		"""Returns a list of anti-affinity policies within a specific location.
+
+		>>> clc.v2.AntiAffinity.GetLocation("VA1")
+		[<clc.APIv2.anti_affinity.AntiAffinity object at 0x105eeded0>]
+
+		"""
+		return(AntiAffinity.GetAll(alias=alias,location=location))
 
 
-	def __init__(self,id,name=None,location=None,
+	def __init__(self,id,name=None,location=None,servers=None):
+		pass
 
+
+	def __str__(self):
+		pass
 
