e8c2eef447af4a21b98b99aea97f55e099fce763
Keith Resar <keith.resar@centurylinkcloud.com>
2015-01-06 10:24:37 -0600

    -

--- src/clc/APIv2/server.py
+++ src/clc/APIv2/server.py
@@ -23,24 +23,24 @@ import clc
 
 class Server(object):
 
-	@staticmethod
-	def GetAll(root_group_id,alias=None):  
-		"""Gets a list of groups within a given account.
-
-		"""
-
-		if not alias:  alias = clc.v2.Account.GetAlias()
-
-		groups = []
-		for r in clc.v2.API.Call('GET','groups/%s/%s' % (alias,root_group_id),{})['groups']:
-			groups.append(Group(id=r['id'],alias=alias,group_obj=r))
-		
-		return(groups)
+#	@staticmethod
+#	def GetAll(root_group_id,alias=None):  
+#		"""Gets a list of groups within a given account.
+#
+#		"""
+#
+#		if not alias:  alias = clc.v2.Account.GetAlias()
+#
+#		groups = []
+#		for r in clc.v2.API.Call('GET','groups/%s/%s' % (alias,root_group_id),{})['groups']:
+#			groups.append(Group(id=r['id'],alias=alias,group_obj=r))
+#		
+#		return(groups)
 
 
 
 	def __init__(self,id,alias=None,group_obj=None):
-		"""Create Group object.
+		"""Create Server object.
 
 		If parameters are populated then create object location.  
 		Else if only id is supplied issue a Get Policy call
@@ -64,38 +64,32 @@ class Server(object):
 		else:  raise(AttributeError("'%s' instance has no attribute '%s'" % (self.__class__.__name__,var)))
 
 
-	def Create(self,name,description=None):  
-		"""Creates a new group
-
-		*TODO* API not yet documented
-
-		>>> clc.v2.Datacenter(location="WA1").RootGroup().Create("Test3","Description3")
-		Request: https://api.tier3.com/v2/groups/BTDI
-		payload={'parentGroupId': u'wa1-837', 'name': 'Test3', 'description': 'Description3'}
-		Response: status_code: 400
-		{"message":"The 'name' property is required."}
-
-		"""
-
-		if not description:  description = name
-
-		#clc.v2.API.Call('POST','groups/%s' % (self.alias),{'name': name, 'description': description, 'parentGroupId': self.id},debug=True)
-		raise(Exception("Not implemented"))
-
-
-	def Update(self):
-		"""Update group
-
-		*TODO* API not yet documented
-
-		"""
-		raise(Exception("Not implemented"))
-
-
-	def Delete(self):
-		"""Delete group."""
-		#status = {u'href': u'/v2/operations/btdi/status/wa1-126437', u'id': u'wa1-126437', u'rel': u'status'}
-		status = clc.v2.API.Call('DELETE','groups/%s/%s' % (self.alias,self.id),{})
+#	def Create(self,name,description=None):  
+#		"""Creates a new group
+#
+#		*TODO* 
+#
+#		"""
+#
+#		if not description:  description = name
+#
+#		#clc.v2.API.Call('POST','groups/%s' % (self.alias),{'name': name, 'description': description, 'parentGroupId': self.id},debug=True)
+#		raise(Exception("Not implemented"))
+#
+#
+#	def Update(self):
+#		"""Update group
+#
+#		*TODO* API not yet documented
+#
+#		"""
+#		raise(Exception("Not implemented"))
+#
+#
+#	def Delete(self):
+#		"""Delete group."""
+#		#status = {u'href': u'/v2/operations/btdi/status/wa1-126437', u'id': u'wa1-126437', u'rel': u'status'}
+#		status = clc.v2.API.Call('DELETE','groups/%s/%s' % (self.alias,self.id),{})
 
 
 	def __str__(self):
