2fb355c2d0a2c0aab1ea0034975d1953d06620bf
Keith Resar <keith.resar@centurylinkcloud.com>
2015-01-09 11:29:36 -0600

    -

--- src/clc/APIv2/__init__.py
+++ src/clc/APIv2/__init__.py
@@ -26,6 +26,7 @@ from clc.APIv2.queue import Queue, Request, Requests
 from clc.APIv2.anti_affinity import AntiAffinity
 from clc.APIv2.datacenter import Datacenter
 from clc.APIv2.api import API
+import clc.APIv2.time_utils
 
 
 ####### module/object vars #######
