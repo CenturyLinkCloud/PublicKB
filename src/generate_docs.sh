84958a368cfd211f327362d9acd50737f98e52dc
Keith Resar <keith.resar@centurylinkcloud.com>
2015-01-09 20:32:37 -0600

    -

--- src/generate_docs.sh
+++ src/generate_docs.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 
-pydoc -w clc clc.APIv1.{account,api,billing,blueprint,cli,group,network,output,queue,server,shell,user}
+pydoc -w clc \
+         clc.APIv1.{account,api,billing,blueprint,cli,group,network,output,queue,server,shell,user} \
+         clc.APIv2.{account,alert,anti_affinity,api,datacenter,group,network,queue,server,template,time_utils}
 perl -p -i -e "s/keithresar//g" *html
 
 echo "Manual step  > git checkout gh-pages"
