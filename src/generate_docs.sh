f9e448ba141c013854644217353cce5f15b5bf24
Keith Resar <keith.resar@centurylinkcloud.com>
2015-01-09 20:35:36 -0600

    -

--- src/generate_docs.sh
+++ src/generate_docs.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 pydoc -w clc \
+		 clc.APIv1 clc.APIv2 \
          clc.APIv1.{account,api,billing,blueprint,cli,group,network,output,queue,server,shell,user} \
          clc.APIv2.{account,alert,anti_affinity,api,datacenter,group,network,queue,server,template,time_utils}
 perl -p -i -e "s/keithresar//g" *html
