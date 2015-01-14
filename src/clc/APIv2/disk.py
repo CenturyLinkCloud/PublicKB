031f32bbc8c1a82e956e8e705138fb016c7774ff
Keith Resar <keith.resar@centurylinkcloud.com>
2015-01-14 12:20:31 -0600

    -

--- src/clc/APIv2/disk.py
+++ src/clc/APIv2/disk.py
@@ -5,10 +5,12 @@ Disks object variables:
 
 Disk object variables:
 
-	disk.name
-	disk.id (alias of name)
+	disk.id 
+	disk.partition_paths - list of mounts paths
+	disk.size - disk size in GB
 
 """
+	#{u'id': u'0:0', u'partitionPaths': [], u'sizeGB': 1}
 
 
 import clc
@@ -35,31 +37,34 @@ class Disks(object):
 
 
 	def Search(self,key):
-		"""Search disk list by providing partial mount point or ID
+		"""Search disk list by partial mount point or ID
 
 		"""
 
 		results = []
 		for disk in self.disks:
 			if disk.id.lower().find(key.lower()) != -1:  results.append(disk)
-			elif disk.name.lower().find(key.lower()) != -1:  results.append(disk)
+			# TODO - search in list to match partial mount points
+			elif key.lower() in disk.partition_paths:  results.append(disk)
 
 		return(results)
 
 
 class Disk(object):
 
-	#{u'id': u'0:0', u'partitionPaths': [], u'sizeGB': 1}
 	def __init__(self,id,disk_obj=None):
 		"""Create Disk object."""
 
 		self.id = id
+		self.size = disk_obj['sizeGB']
 		self.data = disk_obj
 
 
 	def __getattr__(self,var):
-		if var in self.data:  return(self.data[var])
-		else:  raise(AttributeError("'%s' instance has no attribute '%s'" % (self.__class__.__name__,var)))
+		key = re.sub("_(.)",lambda pat: pat.group(1).upper(),var)
+
+		if key in self.data:  return(self.data[key])
+		else:  raise(AttributeError("'%s' instance has no attribute '%s'" % (self.__class__.__name__,key)))
 
 
 	def __str__(self):
