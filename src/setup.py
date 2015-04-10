9eebd3741cf135cd3c73401aa5ced2555e6f0a4b
Keith Resar <keith.resar@centurylinkcloud.com>
2015-04-10 11:42:48 -0500

    -

--- src/setup.py
+++ src/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "clc-sdk",
-	version = "2.9",
+	version = "2.10",
 	packages = find_packages("."),
 
 	install_requires = ['prettytable','clint','argparse','requests'],
